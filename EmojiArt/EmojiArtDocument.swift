cd6c91aba206c87333302deb3c35691bc1fd2280
bogdanov <iceposhta@gmail.com>
2021-04-23 22:58:41 +0100

    EmojiArtDocument functions to call from a view

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -8,7 +8,31 @@
 import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
-    
     static let palette: String = "❤️🎎✈️🏡🐥🏄"
+
+    @Published
+    private var emojiArt = EmojiArt()
+
+    // MARRK: - Intents(s)
+
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
     
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url
+    }
 }
