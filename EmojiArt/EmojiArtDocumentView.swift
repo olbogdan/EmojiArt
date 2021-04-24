f6152271d57035503f3e325dae695cb5f382fcca
bogdanov <iceposhta@gmail.com>
2021-04-24 12:46:00 +0100

    listen for for emoji drop event

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -21,25 +21,35 @@ struct EmojiArtDocumentView: View {
                 }
             }
             .padding(.horizontal)
-            Rectangle().foregroundColor(.white).overlay(
-                Group {
-                    if let image = self.document.backgroundImage {
-                        Image(uiImage: image)
+            GeometryReader { geometry in
+                Color.white.overlay(
+                    Group {
+                        if let image = self.document.backgroundImage {
+                            Image(uiImage: image)
+                        }
                     }
+                )
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                    var location = geometry.convert(location, from: .global)
+                    location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                    return drop(providers: providers, at: location)
                 }
-            )
-            .edgesIgnoringSafeArea([.horizontal, .bottom])
-            .onDrop(of: ["public.image"], isTargeted: nil) { providers, _ in
-                drop(providers: providers)
             }
         }
     }
 
-    private func drop(providers: [NSItemProvider]) -> Bool {
-        let found = providers.loadFirstObject(ofType: URL.self) { url in
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
             print("dropped  \(url)")
             document.setBackgroundURL(url)
         }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                document.addEmoji(string, at: location, size: defaultEmojiSize)
+            }
+        }
+
         return found
     }
 
