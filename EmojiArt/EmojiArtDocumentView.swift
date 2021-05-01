6ad7bd2ba08f92c8fe420b4fd6f58c28df30253b
bogdanov <iceposhta@gmail.com>
2021-05-01 23:28:29 +0100

    fix emoji position on a local canvas

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -66,7 +66,7 @@ struct EmojiArtDocumentView: View {
                     zoomToFit(image, in: geometry.size)
                 }
                 .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
-                    var location = geometry.convert(location, from: .global)
+                    var location = geometry.convert(location, from: .local)
                     location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
                     location = CGPoint(x: location.x/zoomScale, y: location.y/zoomScale)
                     return drop(providers: providers, at: location)
