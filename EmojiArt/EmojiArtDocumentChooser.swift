0fbc39a196c8c2adc06be78af4acf541da654b8d
bogdanov <iceposhta@gmail.com>
2021-05-02 00:44:24 +0100

    feature: edit a document name

--- EmojiArt/EmojiArtDocumentChooser.swift
+++ EmojiArt/EmojiArtDocumentChooser.swift
@@ -10,13 +10,17 @@ import SwiftUI
 struct EmojiArtDocumentChooser: View {
     @EnvironmentObject var store: EmojiArtDocumentStore
 
+    @State private var editMode: EditMode = .inactive
+
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.documents) { document in
                     NavigationLink(destination: EmojiArtDocumentView(document: document)
                         .navigationBarTitle(store.name(for: document))) {
-                        Text(store.name(for: document))
+                        EditableText(store.name(for: document), isEditing: editMode.isEditing) { name in
+                            store.setName(name, for: document)
+                        }
                     }
                 }
                 .onDelete { indexSet in
@@ -33,6 +37,7 @@ struct EmojiArtDocumentChooser: View {
                 }),
                 trailing: EditButton()
             )
+            .environment(\.editMode, $editMode)
         }
     }
 }
